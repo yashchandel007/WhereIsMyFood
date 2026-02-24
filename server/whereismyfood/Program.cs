3d34462333c7be7d9be29b462fc5d2471a2762f3
Yash Chandel <yashchandel007@gmail.com>
2026-02-24 19:00:59 +0530

    added scalar to enable ui for api calls

--- server/whereismyfood/Program.cs
+++ server/whereismyfood/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +12,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
